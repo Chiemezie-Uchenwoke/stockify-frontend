cf9921ff476a478f6a8dbe8beb0a876bc4e07983
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-11-15 09:26:13 +0100

    Add Features component file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import HeaderLg from "../components/Header/HeaderLg";
 import HeaderSm from "../components/Header/HeaderSm";
 import Hero from "../components/Hero/Hero";
+import Features from "../components/Features/Features";
 
 const HomePage = () => {
     return (
@@ -8,8 +9,9 @@ const HomePage = () => {
             <HeaderLg />
             <HeaderSm />
             <Hero />
+            <Features />
         </>
     )
-}
+};
 
 export default HomePage;
