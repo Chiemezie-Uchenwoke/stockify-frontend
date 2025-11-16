1cb079631a40945ecb12116f0151d0074d57d21e
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-11-16 16:58:05 +0100

    Add features Card component props and style the card, complete features section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,7 +1,33 @@
+import FeaturesCard from "./FeaturesCard";
+import featuresData from "../../data/featuresData";
+
 const Features = () => {
+    
+
     return (
-        <section>
-        
+        <section className="mt-16 w-full flex flex-col gap-8 items-center">
+            <div className="flex flex-col gap-2">
+                <h2 className="text-center font-bold capitalize text-xl lg:text-3xl">
+                    Powerful Features for Your Business
+                </h2>
+
+                <p className="text-center text-sm lg:text-base">
+                    Everything you need to manage inventory and track profits efficiently
+                </p>
+            </div>
+
+            <div className="container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                {
+                    featuresData.map((feature) => {
+                        return (
+                            <FeaturesCard 
+                                key={feature.id}
+                                {...feature}
+                            />
+                        )
+                    })
+                }
+            </div>
         </section>
     )
 };
--- src/components/Features/FeaturesCard.jsx
+++ src/components/Features/FeaturesCard.jsx
@@ -1,7 +1,42 @@
-const FeaturesCard = () => {
+import { IoCheckmark } from "react-icons/io5";
+import useThemeStore from "../../stores/ThemeStore";
+
+const FeaturesCard = ({title, description, icon: Icon, keyFeatures, bgColor, iconColor}) => {
+    const {theme} = useThemeStore();
+    const currentBgColor = `${theme === "dark" ? bgColor.darkTheme : bgColor.lightTheme}`;
+    const currentIconColor = `${theme === "dark" ? iconColor.darkTheme : iconColor.lightTheme}`;
+
     return (
-        <div>
-        
+        <div className={`${theme === "dark" ? "dark" : ""} border border-black/10 rounded-2xl py-8 px-4 lg:px-6 flex flex-col gap-4 bg-light-surface dark:bg-dark-surface hover:shadow-lg dark:hover:scale-97 duration-200 transition`}>
+            <span 
+                className={`w-10 h-10 lg:w-12 lg:h-12 rounded border flex justify-center items-center border-black/10 text-xl `}
+                style={{backgroundColor: currentBgColor, color: currentIconColor}}
+            >
+                <Icon />
+            </span>
+
+            <h3 className={`font-bold sm:text-lg`}>
+                {title}
+            </h3>
+
+            <p className={`text-light-text-primary/60 dark:text-light-surface/70`}>
+                {description}
+            </p>
+
+            <ul className={`flex flex-col gap-2`}>
+                {
+                    keyFeatures.map((f, i) => {
+                        return (
+                            <li 
+                                key={i} 
+                                className="flex items-center gap-2 text-light-text-primary/60 dark:text-light-surface/70"
+                            >
+                                <IoCheckmark className="text-green-600" /> {f}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 };
