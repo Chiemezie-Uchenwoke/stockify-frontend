c6a2b1b010d01393e0f807c4e020e4516372b646
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-11-17 14:51:34 +0100

    Complete footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import { FaRegCopyright } from "react-icons/fa6";
+import useThemeStore from "../../stores/ThemeStore";
+import Logo from "../Logo/Logo";
+import { Link } from "react-router";
+
+const Footer = () => {
+    const {theme} = useThemeStore();
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <footer className={`${theme === "dark" ? "dark" : ""} bg-dark-bg py-18`}>
+            <div className="container text-white flex flex-col gap-8 items-center md:flex-row md:justify-between">
+                <div className="flex flex-col items-center gap-3 md:items-start">
+                    <Logo 
+                        iconWidth={2} 
+                        iconHeight={2} 
+                        iconSize={1.1} 
+                        logoTextSize={1}
+                    />
+
+                    <p className="text-white-shade/60 text-sm">
+                        Inventory and profit tracking for modern businesses.
+                    </p>
+                </div>
+
+                <div className="flex flex-col gap-3">
+                    <ul className="flex items-center gap-3 text-sm ">
+                        <li className="text-white-shade/60 hover:text-white-shade/80">
+                            <Link to="#">Privacy</Link>
+                        </li>
+                        <li className="text-white-shade/60 hover:text-white-shade/80">
+                            <Link to="#">Terms</Link>
+                        </li>
+                        <li className="text-white-shade/60 hover:text-white-shade/80">
+                            <Link to="#">Contact</Link>
+                        </li>
+                        <li className="text-white-shade/60 hover:text-white-shade/80">
+                            <Link to="#">Support</Link>
+                        </li>
+                    </ul>
+
+                    <p className="flex items-center gap-2 text-sm text-white-shade/60">
+                        <FaRegCopyright /> {currentYear} Stockify. All rights reserved
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import HeaderSm from "../components/Header/HeaderSm";
 import Hero from "../components/Hero/Hero";
 import Features from "../components/Features/Features";
 import Cta from "../components/Cta/Cta";
+import Footer from "../components/Footer/Footer";
 
 const HomePage = () => {
 
@@ -13,6 +14,7 @@ const HomePage = () => {
             <Hero />
             <Features />
             <Cta />
+            <Footer />
         </>
     )
 };
