98b5cd681ff7e784065efc9f5e2dc73e4586e5af
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-11-21 13:07:32 +0100

    Add: Dashboard sidebar button data
    Create sidebar views data. This data will be passed through props to create all dashboard views

---
+++ src/data/sidebarViewsData.js
@@ -0,0 +1,45 @@
+import { RiDashboardFill } from "react-icons/ri";
+import { BsStack } from "react-icons/bs";
+import { FaBox, FaMoneyBillWave, FaReceipt } from "react-icons/fa";
+import { FaChartBar } from "react-icons/fa6";
+
+const sidebarViews = [
+    {
+        id: 1,
+        text: "Dashboard",
+        icon: RiDashboardFill,
+        view: "dashboard",
+    },
+    {
+        id: 2,
+        text: "Batches",
+        icon: BsStack,
+        view: "batches",
+    },
+    {
+        id: 3,
+        text: "Products",
+        icon: FaBox,
+        view: "products",
+    },
+    {
+        id: 4,
+        text: "Sales",
+        icon: FaReceipt,
+        view: "sales",
+    },
+    {
+        id: 5,
+        text: "Expenses",
+        icon: FaMoneyBillWave,
+        view: "expenses",
+    },
+    {
+        id: 6,
+        text: "Reports",
+        icon: FaChartBar,
+        view: "reports",
+    },
+];
+
+export default sidebarViews;
