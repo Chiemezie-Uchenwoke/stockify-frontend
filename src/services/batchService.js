6ae85b2f4456eff06946b8f1582552ee9eb14019
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-12-04 14:24:38 +0100

    Feat: GET all batches
    Add get all batches service

---
+++ src/services/batchService.js
@@ -0,0 +1,30 @@
+import fetchWithAuth from "../utils/fetchWithAuth";
+
+const apiBaseUrl = import.meta.env.VITE_STOCKIFY_BACKEND_URL;
+
+const getAllBatches = async () => {
+    try {
+        const response = fetchWithAuth(apiBaseUrl, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            return {
+                success: false,
+                message: data.message || "Unable to fetch batches",
+            };
+        }
+
+        return data;
+    } catch (err) {
+        console.error("Error getting batches: ", err);
+        throw err;
+    }
+}
+
+export {getAllBatches};
