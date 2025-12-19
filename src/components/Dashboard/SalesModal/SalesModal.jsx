63193c9317e78367fb42fdda7fd42395fef78533
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-12-19 15:10:37 +0100

    Fix: Make modal left and right padding to be 1.25rem

--- src/components/Dashboard/SalesModal/SalesModal.jsx
+++ src/components/Dashboard/SalesModal/SalesModal.jsx
@@ -6,7 +6,7 @@ import FormButton from "../../FormButton/FormButton";
 const SalesModal = ({setIsSalesModalOpen}) => {
     return (
         <div className="w-full h-screen flex justify-center items-center fixed top-0 bg-black/30 left-0 z-[100] dark:bg-black/40">
-            <div className="w-[90%] max-w-80 bg-light-surface h-100 rounded-2xl py-6 px-4 dark:bg-dark-surface overflow-y-auto">
+            <div className="w-[90%] max-w-80 bg-light-surface h-100 rounded-2xl py-6 px-5 dark:bg-dark-surface overflow-y-auto">
                 <form className="flex flex-col gap-4">
                     <div className="flex items-center justify-between">
                         <h3 className="text-sm font-bold dark:text-white-shade/80">Record a Sale</h3>
