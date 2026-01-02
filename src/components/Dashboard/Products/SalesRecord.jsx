e22eb22d97eb73a17807767402cba019a8278b0f
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2026-01-02 11:07:31 +0100

    Fix: Remove Quantity purchased from table and replace with total sale amount

--- src/components/Dashboard/Products/SalesRecord.jsx
+++ src/components/Dashboard/Products/SalesRecord.jsx
@@ -14,15 +14,15 @@ const SalesRecord = ({batchSales}) => {
                     </th>
 
                     <th className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 py-1 px-2 sm:py-2 sm:px-4">
-                        Quantity purchased
-                    </th>
-
-                    <th className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 py-1 px-2 sm:py-2 sm:px-4">
                         Quantity Sold
                     </th>
 
                     <th className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 py-1 px-2 sm:py-2 sm:px-4">
                         Selling Price
+                    </th>
+
+                    <th className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 py-1 px-2 sm:py-2 sm:px-4">
+                        Total Sale Amount 
                     </th>
 
                     <th className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 py-1 px-2 sm:py-2 sm:px-4">
@@ -45,15 +45,15 @@ const SalesRecord = ({batchSales}) => {
                                 </td>
 
                                 <td className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 text-center whitespace-normal break-words py-2 px-2 sm:py-2 sm:px-4 h-12"> 
-                                    {sale?.quantity} 
-                                </td>
-
-                                <td className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 text-center whitespace-normal break-words py-2 px-2 sm:py-2 sm:px-4 h-12"> 
                                     {sale?.quantitySold} 
                                 </td>
 
                                 <td className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 text-center whitespace-normal break-words py-2 px-2 sm:py-2 sm:px-4 h-12"> 
-                                    {new Intl.NumberFormat("en-NG", { style: "currency", currency: "NGN" }).format(sale?.sellingPrice) }      
+                                    {new Intl.NumberFormat("en-NG", { style: "currency", currency: "NGN" }).format(sale?.sellingPrice) } 
+                                </td>
+
+                                <td className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 text-center whitespace-normal break-words py-2 px-2 sm:py-2 sm:px-4 h-12"> 
+                                    {new Intl.NumberFormat("en-NG", { style: "currency", currency: "NGN" }).format(sale?.sellingPrice * sale?.quantitySold) }      
                                 </td>
 
                                 <td className="text-[10px] sm:text-xs border dark:border-white-shade/50 dark:text-white-shade/80 text-center whitespace-normal break-words py-2 px-2 sm:py-2 sm:px-4 h-12">
